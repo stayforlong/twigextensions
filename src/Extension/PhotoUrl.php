4dba7b16a9a6b8b695f2429a73ac2c079da1644f
Raúl <raul.moron@gmail.com>
2016-03-21 13:59:12 +0100

    Add the encrypted param to use encrypted or public path image to get the resource from Amazon S3

--- src/Extension/PhotoUrl.php
+++ src/Extension/PhotoUrl.php
@@ -23,36 +23,66 @@ class PhotoUrl extends Twig_Extension
 	public function getFunctions()
 	{
 		return [
-		  new Twig_SimpleFunction(
-			'photo_url',
-			function ($name) {
-				$arguments = func_get_args();
+			new Twig_SimpleFunction(
+				'photo_url',
+				function ($name) {
+					$arguments = func_get_args();
 
-				$type = $arguments[0];
-				$photo = $arguments[1];
-				$path = $arguments[2];
+					$type      = $arguments[0];
+					$photo     = $arguments[1];
+					$path      = $arguments[2];
+					$encrypted = !empty($arguments[3]) ? $arguments[3] : false;
 
-				if( "stayforlong" == $photo['source'] ){
-					return $this->getUrlBucket($type, $photo['path']);
-				}
+					if ("stayforlong" == $photo['source']) {
+						return $this->getUrlBucket($type, $photo['path'], $encrypted);
+					}
 
-				if(!empty($photo['path'])){
-					return $path.$photo['path'];
-				}
-			}, ['is_safe' => ['html']]
-		  ),
+					if (!empty($photo['path'])) {
+						return $path . $photo['path'];
+					}
+				}, ['is_safe' => ['html']]
+			),
 		];
 	}
 
-	private function getUrlBucket($type, $path)
+	private function getUrlBucket($type, $path, $encrypted = false)
 	{
 		$bucket = config('filesystems.disks.s3.bucket');
-		$command = Storage::getAdapter()->getClient()->getCommand('GetObject', [
-			'Bucket' => $bucket,
-			'Key'    => "$type/$path",
-		]);
 
-		$request = Storage::getAdapter()->getClient()->createPresignedRequest($command, '+5 minutes');
+		if ($encrypted) {
+			$url = $this->getEncryptedUrl($bucket, $type, $path);
+		} else {
+			$url = $this->getPublicUrl($bucket, $type, $path);
+		}
+
+		return $url;
+	}
+
+	/**
+	 * @param $bucket
+	 * @param $type
+	 * @param $path
+	 * @return string
+	 */
+	private function getEncryptedUrl($bucket, $type, $path)
+	{
+		$command = Storage::getAdapter()
+			->getClient()
+			->getCommand('GetObject', [
+					'Bucket' => $bucket,
+					'Key'    => "$type/$path",
+				]
+			);
+
+		$request = Storage::getAdapter()
+			->getClient()
+			->createPresignedRequest($command, '+5 minutes');
+
 		return (string)$request->getUri();
+	}
+
+	private function getPublicUrl($bucket, $type, $path)
+	{
+		return Storage::getAdapter()->getClient()->getObjectUrl($bucket, "$type/$path");
 	}
 }
