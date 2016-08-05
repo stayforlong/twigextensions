d29649716ce1d0553e321d22c7483bee0c4831c5
stayforlong <raul@stayforlong.com>
2016-08-05 18:25:26 +0200

    Update Intl.php
    Fix problems with currency innecessary replacement

--- src/Extension/Intl.php
+++ src/Extension/Intl.php
@@ -93,7 +93,7 @@ class Intl extends Twig_Extension
         $formatter = $this->twig_get_number_formatter($locale, 'currency');
 
         $currency = $formatter->formatCurrency($number, $currency);
-        return str_replace(',00', '', str_replace('.00', '', $currency));
+        return $currency;
     }
 
     /**
